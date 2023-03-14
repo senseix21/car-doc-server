cd4a7e32c6dffee51ef928443edd2680703dd386
senseix21 <mahdix2118@protonmail.com>
2023-03-14 12:37:56 +0600

    added mongo db

--- index.js
+++ index.js
@@ -10,15 +10,35 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-//
+//MongoDb CRUD Setup 
 
-const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_password}@cluster1.bhuozyz.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.bhuozyz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        const serviceCollection = client.db('carDoc').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+
+    finally {
+
+    }
+
+}
+run().catch((error) => console.error(error));
 
 
 
 app.get('/', (req, res) => {
-    res.send('car doc port running at http://localhost:4000')
+    res.send('car doc  running at http://localhost:4000')
 });
 
 app.listen(port, () => {
